296855dbe81912d5d9aac90bd9ebd875dfeeb0e8
tanodam <desariod95@gmail.com>
2020-05-31 17:01:13 -0300

    avanzando poco

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyC_3t-M2jA2Z1DtCkK-FeElYTmhBZQIUNY",
+    authDomain: "clinica-online-73429.firebaseapp.com",
+    databaseURL: "https://clinica-online-73429.firebaseio.com",
+    projectId: "clinica-online-73429",
+    storageBucket: "clinica-online-73429.appspot.com",
+    messagingSenderId: "172530902314",
+    appId: "1:172530902314:web:9a081dc36b713603572a53",
+    measurementId: "G-K9C7X68VSL"
+  }
 };
 
 /*
